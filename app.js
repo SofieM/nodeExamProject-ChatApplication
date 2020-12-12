acf2d3d2fb61a62e4b23e1a9a09ed1a0926aad84
SoMohr <sofi591r@stud.kea.dk>
2020-12-12 15:34:11 +0100

    socket.io implementeret - server og klient

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
 const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
+
 const app = express();
+const server = http.createServer(app);
+//Socket.io skal kaldes med en http-serveren (derfor loades http direkte ind med require - og serveren laves ovenfor)
+//Selv om node bruger den "bagom" lige meget hvad (express bygger på http),
+// er der behov for at have en "ren" http-instans til socketio
+const io = socketio(server);
 
 app.use(express.static('public'));
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.get('/', (req, res) =>{
-    return res.sendFile('index.html');
-});
+io.on('connection', () => {
+    console.log('New Websocket connection');
+})
 
 const port  = process.env.PORT || 3000;
 
-app.listen(port, (error) => {
+server.listen(port, (error) => {
     if(error){
         console.log("Server couldn't start...", error);
     }
-    console.log(`Server started on port ${port}`)
+    console.log(`Server started on port ${port}`);
 });
---
+++ public/js/chat.js
@@ -0,0 +1,2 @@
+//brugen af socket.io på klientsiden (scriptet kaldes i index.html) giver adgang til funktionen io
+io()
