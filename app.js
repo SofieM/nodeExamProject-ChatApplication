35e8bfc2aac9d76b25d179a055a4fa50d475e6f7
SoMohr <sofi591r@stud.kea.dk>
2020-12-12 19:59:48 +0100

    geolocation implementeret

--- app.js
+++ app.js
@@ -11,9 +11,33 @@ const io = socketio(server);
 
 app.use(express.static('public'));
 
-io.on('connection', () => {
+io.on('connection', (socket) => {
     console.log('New Websocket connection');
-})
+    //socket.emit sender event til en specifik klient (til klientens socket)
+    //sender et event = det selvlavede: message og en værdi (en velkomst-string),
+    //Værdien kan bruges som det førte argument til callback funktionen hos klienten
+    socket.emit('message', 'Welcome!');
+
+    //broadcast sender en besked til alle andre end den socket, det udgår fra
+    socket.broadcast.emit('message', 'A new user has joined');
+
+    //modtager eventet sendMessage fra klienten
+    socket.on('sendMessage', (message, callback) => {
+        //io.emit sender event til alle klienter, der er forbundet til serveren
+        io.emit('message', message);
+        //Aknowledgement: den der modtager et event (socket.on), modtager en callback-funktion,
+        //som skal kaldes for at "aknowledge" overfor klienten
+        callback('Delivered!');
+    });
+
+    socket.on('sendLocation', (coords) => {
+        io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+    });
+    
+    socket.on('disconnect', () =>{
+        io.emit('message', 'A user has left!')
+    });
+});
 
 const port  = process.env.PORT || 3000;
 
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,2 +1,36 @@
 //brugen af socket.io på klientsiden (scriptet kaldes i index.html) giver adgang til funktionen io
-io()
+const socket = io();
+
+//modtager event fra serveren: message
+socket.on('message', (message) => {
+    console.log(message);
+});
+
+//Dette skal nok laves med Ajax/fetch i stedet for
+document.querySelector('#chat-form').addEventListener('submit', (e) =>{
+    e.preventDefault();
+    const message = e.target.elements.message.value;
+    
+    //Sender eventet "sendmessage" til serveren
+    //Acknowledgement: den der emitter et event, sætter en callback funktion op
+    socket.emit('sendMessage', message, (message) => {
+        console.log('The message was delivered!', message);
+    });
+});
+
+//sender klientens geolocation, hvis klienten trykker på knappen 'send location'
+document.querySelector('#send-location').addEventListener('click', () => {
+    //hvis klientens browser ikke understøtter geolocation
+   if(!navigator.geolocation) {
+       return alert('Geolocation is not supported by your browser');
+   }
+
+   navigator.geolocation.getCurrentPosition((position) => {
+       socket.emit('sendLocation', {
+           latitude: position.coords.latitude,
+           longitude: position.coords.longitude
+       }, () => {
+           console.log('Location shared!')
+       });
+   });
+});
